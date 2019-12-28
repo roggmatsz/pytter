947e3ffc3321405013eb578f355721f7e15b6ac6
Rogger Matamoros <rogger@Roggers-MacBook-Pro.local>
2019-12-28 16:22:12 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import yaml
+import requests
+import base64
+
+# bearer / application only auth is used to access read-only data
+# oauth 1.0a is needed to do POST-ey stuff
+
+def readYaml():
+    with open('config.yaml') as f:
+        return yaml.safe_load(f)
+
+def makeHeaders(keys):
+    return {'Authorization': 'Basic {}'.format(keys), 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}
+
+def makePostAuthHeaders(token):
+    return {'Authorization' : 'Bearer {}'.format(token['access_token'])}
+
+def makeOAuthHeaders():
+    return {'Authorization': 'OAuth oauth_consumer_key={}, oauth_token={}, oauth_token_secret={}, oauth_version="1.0'}
+
+DO_THIS = False
+
+session_token = {'token_type': 'bearer', 'access_token': 'AAAAAAAAAAAAAAAAAAAAAL5FBAEAAAAAqoth5%2B6cM2dCT78zx%2BAJWAfPI%2FA%3DesoJlBaxUcW0bnSdOGjyG8dcZZOKf6dC4qWEAyYJvYIPylWGl5'}
+
+config = readYaml()
+data = 'grant_type=client_credentials'
+concat_keys = base64.urlsafe_b64encode('{}:{}'.format(config['consumer_key'], config['consumer_secret']).encode('ascii')).decode('ascii')
+
+if session_token == None:
+    request = requests.post(config['token_url'], headers=makeHeaders(concat_keys), data=data)
+    print(request.json())
+
+#if DO_THIS:
+    # get basic information for my twitter account
+params = { 'usernames': 'roggmatz'}
+userRequest = requests.get(\
+    config['users_url'],\
+    headers={'Authorization': 'Bearer {}'.format(session_token['access_token'])},\
+    params=params\
+).json()['data']
+
+if DO_THIS:
+    # get most recent tweet from basic user info
+    twitter_id = userRequest[0]['most_recent_tweet_id']
+    tweetsRequest = requests.get(config['tweets_url'],\
+        headers=makePostAuthHeaders(session_token),\
+        params={'ids': twitter_id})
+
+# get user timeline tweets
+allTweetsRequest = requests.get(config['timeline_url'],\
+    headers=makePostAuthHeaders(session_token),\
+    params={'user_id': userRequest[0]['id'], 'count': 3})
+
