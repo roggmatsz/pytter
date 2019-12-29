84f079aa98d7f8cb83ac0ac1d4d615e61a823629
Rogger Matamoros <rogger@Roggers-MBP.attlocal.net>
2019-12-28 20:42:16 -0500

    Initial add of the Auth class that will supersede existing code dedicated to getting a bearer token. Untested so far.

--- main.py
+++ main.py
@@ -5,6 +5,39 @@ import base64
 # bearer / application only auth is used to access read-only data
 # oauth 1.0a is needed to do POST-ey stuff
 
+class TwitterAuth:
+    def __init__(self, url, secrets, existingToken=None):
+        self.url = url
+        self.existingToken = existingToken
+        self.secrets = secrets
+        self.concatenatedKey = None
+
+    def getAuthToken(self):
+        if self.existingToken is not None:
+            print('Found existing token: {}'.format(self.existingToken))
+            pass
+        
+        # make concatKeys
+        self.concatenatedKey = base64.urlsafe_b64encode('{}:{}'.format(\
+            self.secrets['key'], self.secrets['secret']).encode('ascii')\
+            .decode('ascii'))
+
+        # make request header
+        auth_header = {\
+            'Authorization': 'Basic {}'.format(self.concatenatedKey),\
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'\
+        }
+        data = 'grant_type=client_credentials'
+        
+        # send the request and do code validation
+        request = requests.post(self.url, headers=auth_header, data=data)
+        if request.status_code != 200:
+            print(request.status_code)
+        else:
+            print(request.json()['data'])
+            self.existingToken = request.json()['data']
+            return self.existingToken
+
 def readYaml():
     with open('config.yaml') as f:
         return yaml.safe_load(f)
