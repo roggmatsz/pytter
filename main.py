38c4b01ceb973776db6876268d88fcbccbf936e8
Rogger Matamoros <rogger@Roggers-MBP.attlocal.net>
2019-12-29 22:13:44 -0500

    Added method to get timeline for a given twitter account.

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ class TwitterService(object):
             'Authorization': 'Bearer {}'.format(bearerToken['access_token'])\
         }
 
-    def getUserInfo(self, username='roggmatz', endpoint=''):
+    def getUserInfo(self, username='roggmatz', endpoint='/users'):
         userParams = {'usernames': username}
         request = requests.get(\
-            self.baseUrl + '/users',\
+            self.baseUrl + endpoint,\
             headers=self.authorizationHeader,\
             params=userParams)
 
@@ -29,13 +29,30 @@ class TwitterService(object):
         
         return request.json()[0]
 
-    def getTweet(self, tweetId):
+    def getTweet(self, tweetId, endpoint='/tweets'):
         params = {'ids': tweetId}
         request = requests.get(\
-            self.baseUrl + '/tweets',\
+            self.baseUrl + endpoint,\
             headers=self.authorizationHeader,\
             params=params)
         
+        # TODO: Factor out validation block into separate function
+        if request.status_code != 200:
+            print(request.status_code)
+            raise Exception('Something went wrong in the User request.')
+        else:
+            print(request.json())
+
+        return request.json()
+
+    def getTimeline(self, user_id, count=6):
+        timeline_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+        params = {'user_id': user_id, 'count': count}
+        request = requests.get(\
+            timeline_url,\
+            headers=self.authorizationHeader,\
+            params=params)
+
         # TODO: Factor out validation block into separate function
         if request.status_code != 200:
             print(request.status_code)
